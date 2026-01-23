573fc7f05e9848335097b1e6963a2848aa15ee67
Leinner-Lopez <leinner1006@gmail.com>
2026-01-23 16:11:11 -0500

    Eliminacion de carpetas innecesarias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { Routes } from '@angular/router';
-import { InicioComponent } from './Features/Medico/inicio-component/inicio-component';
-import { PacientesComponent } from './Features/Medico/pacientes-component/pacientes-component';
-import { CitasComponent } from './Features/Medico/citas-component/citas-component';
 import { LoginComponent } from './Features/Auth/login-component/login-component';
-import { DashboardLayoutComponent } from './Components/dashboard-layout-component/dashboard-layout-component';
 import { RegistrationComponent } from './Features/Auth/registration-component/registration-component';
 import { authGuard } from './Core/Guards/auth-guard';
 import { DashboardAdministradorComponent } from './Features/Administrador/dashboard-administrador-component/dashboard-administrador-component';
@@ -40,16 +36,6 @@ export const routes: Routes = [
         data: { roles: 'ROLE_PACIENTE' },
         children:[
 
-        ]
-    },
-    {
-        path: 'inicio',
-        component: DashboardLayoutComponent,
-        canActivate: [authGuard],
-        children: [
-            { path: '', component: InicioComponent },
-            { path: 'pacientes', component: PacientesComponent },
-            { path: 'citas', component: CitasComponent }
         ]
     },
 ];
